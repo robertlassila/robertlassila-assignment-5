3d1f7d2f32b1e5d4f35ffbc88d765eed54c9ba84
robertlassila <rlassila42@gmail.com>
2024-12-26 16:00:34 -0800

    Variable update
    - Removed redundant variables and replaced them with existing functions in java to make the code more concise

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -5,18 +5,15 @@ import java.util.Arrays;
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
 
-	int numberOfAdds = 0;
-	int addLimit = 10;
 	int index = 0;
 	@Override
 	public boolean add(T item) {
-		if (index < addLimit) {
+		if (index < items.length) {
 			items[index] = item;
 			index++;
 			return true;
-		} else if (index >= addLimit) {
-			addLimit = addLimit * 2;
-			Object[] newArray = Arrays.copyOf(items, addLimit); 
+		} else if (index >= items.length) {
+			Object[] newArray = Arrays.copyOf(items, (items.length * 2)); 
 			items = newArray;
 			items[index] = item;
 			index++;
